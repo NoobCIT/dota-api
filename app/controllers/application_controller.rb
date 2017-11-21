aed603d1e502ea4392f5e74f61e3814969059e91
Kevin <nguyenk215@yahoo.com>
2017-11-20 16:23:16 -0800

    Adds static pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def show
+     @stats = Dota::Match.new(params[:match_id]).stats
+   end
 end
---
+++ lib/dota/data.rb
@@ -0,0 +1,31 @@
+module Dota
+
+  class Data
+
+    def initialize(account_id)
+      @account_id = account_id
+    end
+
+    def matches
+      response = HTTParty.get("https://api.opendota.com/api/players/#{@account_id}/matches")
+      JSON.parse response.body
+    end
+
+    def record
+      response = HTTParty.get("https://api.opendota.com/api/players/#{@account_id}/wl")
+      JSON.parse response.body
+    end
+
+    def mmr
+      response = HTTParty.get("https://api.opendota.com/api/players/#{@account_id}")
+      JSON.parse response.body
+    end
+
+    def heroes
+      response = HTTParty.get("https://api.opendota.com/api/players/#{@account_id}/heroes")
+      JSON.parse response.body
+    end
+
+  end
+
+end
